f38689e851695efd0978a587efcd4f74571f33d4
ninjafiveo <42717162+ninjafiveo@users.noreply.github.com>
2019-12-10 07:51:50 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+/*eslint-env browser*/
+
+//function myFunction(p1, p2) {
+//    "use strict";
+//    return p1 * p2;
+//}
+//document.getElementById("demo").innerHTML = myFunction(4, 3);
+//
+//let charName;
+//let charClass;
+//let adventure = true;
+//let charAge;
+//
+//function(charName, charClass, charAge)
+//{
+//
+//}
+
+var message = "\'Hello\', world";
+alert(message);
+
+let message2 = message + 5;
+alert(message2);
+
+alert(message.length);
